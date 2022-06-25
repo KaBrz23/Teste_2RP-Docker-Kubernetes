613941ee4106e7e2e90ba66e7b1432f8d5ec6993
Kauê Fernandes Braz <kauefernandesbraz@gmail.com>
2022-06-25 20:53:24 -0300

    atualizado

---
+++ variavel.py
@@ -0,0 +1,10 @@
+import os
+import time
+
+TWORPTEST = os.getenv('true100%')
+
+def tworptest_var():
+    print(TWORPTEST)
+while True:
+    tworptest_var()
+    time.sleep(20) #tempo de 20 segundos
